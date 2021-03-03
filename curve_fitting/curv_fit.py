24acf92964b54be350deb6db5182611d604c67bc
ericcar97 <78977980+ericcar97@users.noreply.github.com>
2021-03-03 09:23:05 -0300

    Add files via upload

--- curve_fitting/curv_fit.py
+++ curve_fitting/curv_fit.py
@@ -8,11 +8,11 @@ from numpy import *
 from tabulate import tabulate
 
 ###Importing and cleaning the data
-filename = 'vC_RC.csv'
-data = pd.read_csv(filename)
-column_name = data.columns[1]
-data[column_name] = -data[column_name] #The multimeter got an inverse measure
-fig = px.scatter(data, y = column_name)
+file = 'vC_RC'
+data = pd.read_csv(file + '.csv')
+column = data.columns
+data[column[1]] = -data[column[1]] #The multimeter got an inverse measure
+fig = px.scatter(data, y = column[1])
 fig.show()
 print('From: ')
 ni = input()
@@ -20,10 +20,10 @@ print('To: ')
 nf = input()
 data = data.loc[ni:nf]
 print(data)
-xdata = data['time'].values
-xdata = xdata - xdata[0]
-ydata = data[column_name].values
-yerror = 0.05*ydata
+x_data = data[column[0]].values
+x_data = x_data - x_data[0]
+y_data = data[column[1]].values
+y_error = 0.05*y_data
 datalen = len(data) -1
 
 ###Defying the model
@@ -33,14 +33,14 @@ def V_model(t,V,tau):
 ###Curve fitting
 V0, tau0 = 3. , 1.
 init_values = [V0,tau0]
-fit, covariance = curve_fit(V_model,xdata,ydata,
+fit, covariance = curve_fit(V_model,x_data,y_data,
                             p0 = init_values,
                             absolute_sigma = True,
-                            sigma = yerror)
+                            sigma = y_error)
 error = sqrt(diag(covariance))
-residuals = ydata - V_model(xdata,fit[0],fit[1])
-xmin = (xdata[0]- xdata[1])*2
-xmax = xdata[datalen]+(xdata[datalen] - xdata[datalen - 1])*2
+residuals = y_data - V_model(x_data,fit[0],fit[1])
+xmin = (x_data[0]- x_data[1])*2
+xmax = x_data[datalen]+(x_data[datalen] - x_data[datalen - 1])*2
 xfit = linspace(xmin,xmax,1000)
 yfit = V_model(xfit,fit[0],fit[1])
 
@@ -57,12 +57,11 @@ def plotStyle():
  
 #Subplot: fitted function
 ax1 = fig.add_subplot(gs[0])
-ax1.set_title('Ajuste no lineal')
 ax1.set_ylabel('Voltaje [V]')
-ax1.scatter(xdata,ydata,c = 'r',s = 0.5)
-ax1.errorbar(xdata,ydata, ls = 'none',
+ax1.scatter(x_data,y_data,c = 'r',s = 0.5)
+ax1.errorbar(x_data,y_data, ls = 'none',
              c = 'k',
-             yerr = yerror,
+             yerr = y_error,
              elinewidth= 0.5,
              capsize = 1,
              capthick = 0.5)
@@ -71,10 +70,12 @@ ax1.plot(xfit,yfit,lw = 0.5,
 #ax1.set_xticklabels([])
 plotStyle()
 
-#Subplot:Residuals
+
+
+#Subplot: Residuals
 ax2 = fig.add_subplot(gs[1])
-ax2.scatter(xdata,residuals,c = 'r')
+ax2.scatter(x_data,residuals,c = 'r')
 ax2.set_xlabel('Tiempo [s]')
 ax2.set_ylabel('Residuos')
 plotStyle()
-plt.savefig(column_name + '_RC.eps')
+plt.savefig(file + '.eps')
