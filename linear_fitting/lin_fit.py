897766daefa2cacd9ae23281c29283b465ffb526
ericcar97 <78977980+ericcar97@users.noreply.github.com>
2021-03-03 09:25:09 -0300

    Add files via upload

--- linear_fitting/lin_fit.py
+++ linear_fitting/lin_fit.py
@@ -5,13 +5,14 @@ import plotly.express as px
 from matplotlib import rc
 from lin_fit_mod import lin_fit_mod
 from numpy import *
+from scipy.stats import t
 
 #importing and cleaning the data
-file = 'data.csv'
-data = pd.read_csv(file)
+file = 'data'
+data = pd.read_csv(file + 'csv')
 column = data.columns
 fig = px.scatter(data,y = column)
-#fig.show()
+fig.show()
 print('From: ')
 ni = input()
 print('To: ')
@@ -23,13 +24,12 @@ y_error = data[column[2]].values
 data_len = len(data)
 
 # Linear Regression
-#a, b, sa, sb, r_squared, sigma  = lin_fit_mod.w_lin_fit(x_data, y_data, y_error, data_len)
-a, b, sa, sb, r_squared, sigma  = lin_fit_mod.s_lin_fit(x_data, y_data, data_len)
-print(a, b, sa ,sb, r_squared)
+#a, b, sa, sb, r_squared, sigma  = lin_fit_mod.w_lin_fit(x_data, y_data, y_error, data_len)  #Weighted linear regression
+a, b, sa, sb, r_squared, sigma  = lin_fit_mod.s_lin_fit(x_data, y_data, data_len)   #Simple linear regression
 residuals = y_data - (a*x_data + b)
 
-#plotting
 data_len = data_len - 1
+#plotting
 x_min = x_data[0] - (x_data[1] - x_data[0])/2
 x_max = x_data[data_len] + (x_data[data_len]- x_data[data_len - 1])/2
 x_fit = linspace(x_min,x_max,1000)
@@ -46,7 +46,6 @@ def plotStyle():
     plt.xlim(x_min,x_max)
 
 ax1 = fig.add_subplot(gs[0])
-ax1.set_title('Ajuste Lineal')
 ax1.set_ylabel('Variable dependiente')
 ax1.scatter(x_data, y_data, c = 'r', s = 0.5)
 ax1.errorbar(x_data, y_data , yerr = y_error,
@@ -61,4 +60,4 @@ ax2.set_xlabel('Variable dependiente')
 ax2.set_ylabel('Residuos')
 ax2.scatter(x_data, residuals, c = 'r')
 plotStyle()
-plt.savefig('plot.eps')
+plt.savefig(file + '.eps')
